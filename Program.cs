61c67508c1292d0817c2e19127c3a4355157aca1
daescucristi03 <daescucristi03@gmail.com>
2023-03-22 01:55:18 -0700

    initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Calculator {
+    class Program {
+        // addition and subtraction methods
+        private int addition(int a, int b) { return a + b; }
+        private double addition(double a, double b) { return a + b; }
+        // multiplication and division functions
+        private int multiplication(int a, int b) { return a * b; }
+        private double multiplication(double a, double b) { return a * b; }
+        private double division(double a, double b) {
+            try {
+                return a / b;
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+                return 0;
+            }
+        }
+
+        static void Main(string[] args) {
+
+        }
+    }
+}
