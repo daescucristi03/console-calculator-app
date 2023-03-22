e2658b7bf1c4119c249da76e716b015a303d6156
daescucristi03 <daescucristi03@gmail.com>
2023-03-22 04:01:43 -0700

    v00.1

--- Program.cs
+++ Program.cs
@@ -1,12 +1,11 @@
 ﻿using System;
+using System.Linq;
 
 namespace Calculator {
     class Program {
         // addition and subtraction methods
-        private int addition(int a, int b) { return a + b; }
         private double addition(double a, double b) { return a + b; }
         // multiplication and division functions
-        private int multiplication(int a, int b) { return a * b; }
         private double multiplication(double a, double b) { return a * b; }
         private double division(double a, double b) {
             try {
@@ -17,8 +16,53 @@ namespace Calculator {
             }
         }
 
-        static void Main(string[] args) {
 
+        static void Main(string[] args) {
+            // create an instance of the program
+            Program prog = new Program();
+            Console.Clear();
+
+            double result = 0;
+            bool isRunning = true;
+
+            while(isRunning) {
+                Console.WriteLine("Enter a number:");
+                double num1 = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter an operator (+, -, *, /):");
+                string op = Console.ReadLine();
+
+                Console.WriteLine("Enter another number:");
+                double num2 = double.Parse(Console.ReadLine());
+
+                switch (op) {
+                    case "+":
+                        result = prog.addition(num1, num2);
+                        break;
+                    case "-":
+                        result = prog.addition(num1, num2 * (-1));
+                        break;
+                    case "*":
+                        result = prog.multiplication(num1, num2);
+                        break;
+                    case "/":
+                        result = prog.division(num1, num2);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid operator");
+                        break;
+                }
+
+                Console.WriteLine($"Result: {result}");
+
+                Console.WriteLine("Do you want to continue? (y/n)");
+                string answer = Console.ReadLine();
+
+                if (answer.ToLower() == "n") {
+                    isRunning = false;
+                }
+                Console.Clear();
+            }
         }
     }
 }
