d6f965749135c25957e5d89fd84566368f33138c
daescucristi03 <daescucristi03@gmail.com>
2023-03-27 03:11:29 -0700

    solved warnings and added a few comments

--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ namespace Calculator {
         private double multiplication(double a, double b) { return a * b; }
         private double division(double a, double b) {
             try {
+                // handle in case of division by zero
                 return a / b;
             } catch (Exception e) {
                 Console.WriteLine(e.Message);
@@ -21,41 +22,49 @@ namespace Calculator {
             bool isRunning = true;
 
             while(isRunning) {
+                // get first number
                 Console.WriteLine("Enter a number:");
-                double num1 = double.Parse(Console.ReadLine());
+                double num1 = double.Parse(Console.ReadLine() ?? "0");
 
+                // get operator
                 Console.WriteLine("Enter an operator (+, -, *, /):");
-                string op = Console.ReadLine();
+                string? op = Console.ReadLine();
 
+                // get second number
                 Console.WriteLine("Enter another number:");
-                double num2 = double.Parse(Console.ReadLine());
+                double num2 = double.Parse(Console.ReadLine() ?? "0");
 
                 switch (op) {
                     case "+":
+                    // handle addition
                         result = addition(num1, num2);
                         break;
                     case "-":
+                    // handle subtraction
                         result = addition(num1, num2 * (-1));
                         break;
                     case "*":
+                    // handle multiplication
                         result = multiplication(num1, num2);
                         break;
                     case "/":
+                    // handle division
                         result = division(num1, num2);
                         break;
                     default:
+                    // handle default case
                         Console.WriteLine("Invalid operator");
                         break;
                 }
 
+                // display result
                 Console.WriteLine($"Result: {result}");
 
                 Console.WriteLine("Do you want to continue? (y/n)");
-                string answer = Console.ReadLine();
+                string? answer = Console.ReadLine();
 
-                if (answer.ToLower() == "n") {
-                    isRunning = false;
-                }
+                if (answer?.ToLower() == "n") { isRunning = false; }
+
                 Console.Clear();
             }
         }
@@ -66,6 +75,7 @@ namespace Calculator {
             Program prog = new Program();
             Console.Clear();
 
+            // handle main function
             prog.handleUserInput();
             
         }
