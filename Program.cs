95027fb270072506334f4333f7a1c871fa1f2a5f
daescucristi03 <daescucristi03@gmail.com>
2023-03-27 10:56:24 -0700

    optimizations and comments added

--- Program.cs
+++ Program.cs
@@ -1,27 +1,19 @@
 ﻿using System;
-using System.Linq;
 
 namespace Calculator {
     class Program {
-        // addition and subtraction methods
-        private double addition(double a, double b) { return a + b; }
-        // multiplication and division functions
-        private double multiplication(double a, double b) { return a * b; }
-        private double division(double a, double b) {
-            try {
-                // handle in case of division by zero
-                return a / b;
-            } catch (Exception e) {
-                Console.WriteLine(e.Message);
-                return 0;
-            }
-        }
+        // create delegates for addition, subtraction, multiplication and division
+        private delegate double BinaryOperation(double a, double b);
+        private BinaryOperation addition = (a, b) => a + b;
+        private BinaryOperation subtraction = (a, b) => a - b;
+        private BinaryOperation multiplication = (a, b) => a * b;
+        private BinaryOperation division = (a, b) => b == 0 ? throw new DivideByZeroException("Division by zero") : a / b;
 
         private void handleUserInput() {
             double result = 0;
             bool isRunning = true;
 
-            while(isRunning) {
+            while (isRunning) {
                 // get first number
                 Console.WriteLine("Enter a number:");
                 double num1 = double.Parse(Console.ReadLine() ?? "0");
@@ -34,41 +26,33 @@ namespace Calculator {
                 Console.WriteLine("Enter another number:");
                 double num2 = double.Parse(Console.ReadLine() ?? "0");
 
-                switch (op) {
-                    case "+":
-                    // handle addition
-                        result = addition(num1, num2);
-                        break;
-                    case "-":
-                    // handle subtraction
-                        result = addition(num1, num2 * (-1));
-                        break;
-                    case "*":
-                    // handle multiplication
-                        result = multiplication(num1, num2);
-                        break;
-                    case "/":
-                    // handle division
-                        result = division(num1, num2);
-                        break;
-                    default:
-                    // handle default case
-                        Console.WriteLine("Invalid operator");
-                        break;
+                // create a dictionary with the operations
+                var operations = new Dictionary<string, BinaryOperation>() {
+                    { "+", addition },
+                    { "-", subtraction },
+                    { "*", multiplication },
+                    { "/", division }
+                };
+
+                // if the operator is valid, perform the operation
+                if (operations.TryGetValue(op, out BinaryOperation? operation)) {
+                    result = operation(num1, num2);
+                    Console.WriteLine($"Result: {result}");
+                } else {
+                    Console.WriteLine("Invalid operator");
                 }
 
-                // display result
-                Console.WriteLine($"Result: {result}");
-
+                // ask user if they want to continue
                 Console.WriteLine("Do you want to continue? (y/n)");
                 string? answer = Console.ReadLine();
 
-                if (answer?.ToLower() == "n") { isRunning = false; }
+                if (answer?.ToLower() == "n") {
+                    isRunning = false;
+                }
 
                 Console.Clear();
             }
         }
-
 
         static void Main(string[] args) {
             // create an instance of the program
@@ -77,7 +61,6 @@ namespace Calculator {
 
             // handle main function
             prog.handleUserInput();
-            
         }
     }
 }
